d7de2d989de7acf1f5b2a33aa8c6592bafeafc30
BlushandGlow <blushglow.em@gmail.com>
2024-12-11 12:56:01 -0400

    Add files via upload

---
+++ zcripts.js
@@ -0,0 +1,5 @@
+// scripts.js
+document.getElementById('form-reserva').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('¡Cita reservada con éxito! Nos pondremos en contacto contigo.');
+});
